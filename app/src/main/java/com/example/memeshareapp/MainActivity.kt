8c426449ce3c457394a15044b7fadf526cdcdd0b
lakshay <lakshaykaushik2212@gmail.com>
2024-06-19 15:01:31 +0530

    t

---
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.memeshareapp
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.service.chooser.ChooserAction
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        loadMeme()
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            loadMeme()
+        }
+
+
+    }
+    private fun loadMeme(){
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
+        val image = findViewById<ImageView>(R.id.imageView)
+        val queue = MySingleton.getInstance(this.applicationContext).requestQueue
+        val url = "https://meme-api.com/gimme"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,url,null,
+            { response ->
+                val currenturl = response.getString("url")
+                val button2 = findViewById<Button>(R.id.Button2)
+                button2.setOnClickListener{
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.type = "text/plain"
+                    intent.putExtra(Intent.EXTRA_TEXT,"hey,check this cool meme i got from reddit $currenturl")
+                    val chooser = Intent.createChooser(intent,"share this meme using...")
+                    startActivity(intent)
+                }
+                Glide.with(this).load(currenturl).listener(object : RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        model: Any,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                }).into(image)
+            },
+            {
+                Toast.makeText(this,"something went wrong",Toast.LENGTH_LONG).show()
+            })
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+}
