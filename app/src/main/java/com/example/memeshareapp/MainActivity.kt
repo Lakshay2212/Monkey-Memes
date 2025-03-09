fe5e6e6d1a1b29bff552f4ef2fb9088c5e8d9603
Lakshay2212 <lakshaykaushik2212@gmail.com>
2025-03-09 16:00:46 +0530

    initial commit

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -31,11 +31,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         loadMeme()
         val button1 = findViewById<Button>(R.id.button1)
         button1.setOnClickListener {
--- app/src/main/java/com/example/memeshareapp/splash_screen.kt
+++ app/src/main/java/com/example/memeshareapp/splash_screen.kt
@@ -13,14 +13,11 @@ class splash_screen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
             finish()
-        },1500)
+        },2000)
+
     }
 }
